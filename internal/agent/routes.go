996c2e036b6a59b4566408eb49acbc41cb63ab20
Julien Klaer <klaer.julien@gmail.com>
2024-08-06 22:15:17 +0200

    adding route to execute recipe

--- internal/agent/routes.go
+++ internal/agent/routes.go
@@ -1,6 +1,7 @@
 package agent
 
 import (
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"net/http"
@@ -14,14 +15,30 @@ func (s *server) RegisterRoutes() http.Handler {
 	r := e.Group("/v1")
 	api := r.Group("/agent")
 
-	api.GET("/info", s.HelloWorldHandler)
+	api.GET("/info", s.GetInformationHandler)
+	api.POST("/recipe", s.ExecuteRecipeHandler)
 
 	return e
 }
 
-func (s *server) HelloWorldHandler(c echo.Context) error {
+func (s *server) GetInformationHandler(c echo.Context) error {
 	resp := map[string]string{
 		"message": "Hello World",
+	}
+	return c.JSON(http.StatusOK, resp)
+}
+
+func (s *server) ExecuteRecipeHandler(c echo.Context) error {
+	var r ExecuteRecipeRequest
+
+	err := json.NewDecoder(c.Request().Body).Decode(&r)
+	if err != nil {
+		return err
+	}
+
+	resp := map[string]string{
+		"name":              r.Name,
+		"kernel_parameters": r.KernelParameters[0].Value,
 	}
 	return c.JSON(http.StatusOK, resp)
 }
--- internal/agent/types.go
+++ internal/agent/types.go
@@ -1,4 +1,14 @@
 package agent
 
+type KernelParameter struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type ExecuteRecipeRequest struct {
+	Name             string            `json:"name"`
+	KernelParameters []KernelParameter `json:"kernel_parameters"`
+}
+
 type Agent struct {
 }
